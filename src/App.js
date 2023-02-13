9ffa1e1019cbff99fafdaa6f7d5d4ba317a17825
marvynmesquita <31486769+marvynmesquita@users.noreply.github.com>
2023-02-13 17:04:32 -0300

    changed homepage url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <Navbar/>
           <Routes>
-              <Route exact path="/*" element={<Home />} > </Route>
+              <Route exact path="/" element={<Home />} > </Route>
               <Route exact path="/newproduct" element={<Newproduct />} > </Route>
               <Route exact path="/updateproduct" element={<Updateproduct />} > </Route>
           </Routes>
